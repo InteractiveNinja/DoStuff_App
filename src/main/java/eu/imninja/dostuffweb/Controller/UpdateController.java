ef7f6fb96db3fdf80328dd2d253d99079ef39952
interactiveninja <gabrielnadolny2@gmail.com>
2020-09-29 12:28:40 +0200

    Added UpdateController to Update a Single Entry in the DB, fixed Select Option not showing the right field

---
+++ src/main/java/eu/imninja/dostuffweb/Controller/UpdateController.java
@@ -0,0 +1,47 @@
+package eu.imninja.dostuffweb.Controller;
+
+import eu.imninja.dostuffweb.DAO.TaskDAO;
+import eu.imninja.dostuffweb.DAO.TaskerDAO;
+import eu.imninja.dostuffweb.DAO.WiederholungenDAO;
+import eu.imninja.dostuffweb.Repository.TaskRepository;
+import eu.imninja.dostuffweb.Repository.TaskerRepository;
+import eu.imninja.dostuffweb.Repository.WiederholungenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+
+@Controller
+public class UpdateController {
+
+    @Autowired
+    TaskerRepository taskerRepository;
+    @Autowired
+    WiederholungenRepository wiederholungenRepository;
+    @Autowired
+    TaskRepository taskRepository;
+
+    @PostMapping(value = "/update")
+    public String updateByValues(@RequestParam int id,@RequestParam String beschreibung, @RequestParam String zuerledigen, @RequestParam int tasker_id, @RequestParam int wiederholungs_id) throws ParseException {
+
+        Optional<TaskerDAO> d = taskerRepository.findById(tasker_id);
+        Optional<WiederholungenDAO> w = wiederholungenRepository.findById(wiederholungs_id);
+        Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(zuerledigen.replace("T", " ") + ":00");
+        TaskDAO task = new TaskDAO();
+        task.setId(id);
+        task.setBeschreibung(beschreibung);
+        task.setErledigt(false);
+        task.setZuerledigen(date);
+        task.setTasker_id(d.get());
+        task.setWiederholungen_id(w.get());
+        taskRepository.save(task);
+
+        return "update";
+
+    }
+}
