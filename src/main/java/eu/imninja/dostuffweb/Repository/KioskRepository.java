c7cdd0801d9414b21635cd9c95a375a8b6044607
interactiveninja <gabrielnadolny2@gmail.com>
2020-10-01 11:24:17 +0200

    Deleted Unused Stuff

--- src/main/java/eu/imninja/dostuffweb/Repository/KioskRepository.java
+++
@@ -1,7 +0,0 @@
-package eu.imninja.dostuffweb.Repository;
-
-import eu.imninja.dostuffweb.DAO.KioskDAO;
-import org.springframework.data.repository.CrudRepository;
-
-public interface KioskRepository extends CrudRepository<KioskDAO, Integer> {
-}
