542bf28395cf3d6be5e98a5479d5d58a0371551d
InteractiveNinja <gabrielnadolny2@gmail.com>
2020-10-01 16:33:08 +0200

    edited starter.sh

--- starter.sh
+++ starter.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-[ ! -d "target" ] mvn install
+if [ ! -d "target" ]; then
+mvn install
+fi
 screen -mS doStuff java -jar target/dostuff-web-1.0.0.jar --spring.config.location=target/classes/application.properties
