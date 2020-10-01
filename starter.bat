bff8f10b1edbe4c16baa9e30320ea023ef2e7080
interactiveninja <gabrielnadolny2@gmail.com>
2020-10-01 07:33:45 +0200

    changes in starter.bat

--- starter.bat
+++ starter.bat
@@ -3,6 +3,7 @@ IF exist target (
 echo Projekt schon gebuilded, starte
  ) ELSE (
  mvnw.cmd install
+ java -jar target\dostuff-web-1.0.0.jar --spring.config.location=target\classes\application.properties
  )
 
 java -jar target\dostuff-web-1.0.0.jar --spring.config.location=target\classes\application.properties
