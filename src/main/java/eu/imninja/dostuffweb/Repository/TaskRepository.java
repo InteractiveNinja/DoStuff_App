c15c37bb1f8dc9568c79d24b28b4d0fe30c4b70e
InteractiveNinja <gabrielnadolny2@gmail.com>
2020-10-30 07:52:33 +0100

    Fixed weird bug afecting that timestamps wont get reconized

--- src/main/java/eu/imninja/dostuffweb/Repository/TaskRepository.java
+++ src/main/java/eu/imninja/dostuffweb/Repository/TaskRepository.java
@@ -11,23 +11,23 @@ import java.util.Set;
 public interface TaskRepository extends CrudRepository<TaskDAO, Integer> {
 
     // Hole alle von heute und sortiert dieses nach erledigt und bis wann
-    @Query(value = "SELECT * FROM task t where t.zuerledigen >= CURDATE() and t.zuerledigen < CURDATE()+1 order by t.erledigt, t.zuerledigen",nativeQuery = true)
+    @Query(value = "SELECT * FROM task t where DATE(t.zuerledigen) >= CURDATE() and DATE(t.zuerledigen) < CURDATE()+1 order by t.erledigt, t.zuerledigen",nativeQuery = true)
     public Set<TaskDAO> getAllToday();
 
     // Hole alle von heute die keine wiederholung haben
-    @Query(value = "SELECT * FROM task t where t.zuerledigen >= CURDATE() and  t.zuerledigen < CURDATE()+1 and t.wiederholungen_id = 1",nativeQuery = true)
+    @Query(value = "SELECT * FROM task t where DATE(t.zuerledigen) >= CURDATE() and  DATE(t.zuerledigen) < CURDATE()+1 and t.wiederholungen_id = 1",nativeQuery = true)
     public Set<TaskDAO> getAllWithRepeatNever();
 
     // Hole alle von heute die eine Tägliche wiederholung haben
-    @Query(value = "SELECT * FROM task t where t.zuerledigen >= CURDATE() and  t.zuerledigen < CURDATE()+1 and t.wiederholungen_id = 2",nativeQuery = true)
+    @Query(value = "SELECT * FROM task t where DATE(t.zuerledigen) >= CURDATE() and  DATE(t.zuerledigen) < CURDATE()+1 and t.wiederholungen_id = 2",nativeQuery = true)
     public Set<TaskDAO> getAllWithRepeatDaily();
 
     // Hole alle von heute die eine Wöchentliche wiederholung haben
-    @Query(value = "SELECT * FROM task t where t.zuerledigen >= CURDATE() and  t.zuerledigen < CURDATE()+1 and t.wiederholungen_id = 3",nativeQuery = true)
+    @Query(value = "SELECT * FROM task t where DATE(t.zuerledigen) >= CURDATE() and  DATE(t.zuerledigen) < CURDATE()+1 and t.wiederholungen_id = 3",nativeQuery = true)
     public Set<TaskDAO> getAllWithRepeatWeekly();
 
     // Hole alle die Älter als Heute sind
-    @Query(value = "SELECT * FROM task t where t.zuerledigen < CURDATE()",nativeQuery = true)
+    @Query(value = "SELECT * FROM task t where DATE(t.zuerledigen) < CURDATE()",nativeQuery = true)
     public Set<TaskDAO> getAllFromYesterday();
 
     // Hole alle und Ordne diese per Wiederholungs_id und ob es erledigt ist
